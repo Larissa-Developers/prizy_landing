75f735d9927f7d8d1fb90e187cac2067495f1ddf
ir3n <iren.borada@gmail.com>
2018-11-04 19:06:01 +0200

    experimented with homepage and added logo in header

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 import '../assets/styles/default.css'
 import styles from './header.module.css'
+import logo from '../images/logo.png'
 
 const Header = ({ siteTitle, menuLinks }) => (
     <div className={styles.outer}>
         <div className={styles.wrapper}>
-            <div className={styles.logo}>
-                /*img here*/
-            </div>
+            <img className={styles.logo} src={logo} alt="Logo"/>
             <nav className={styles.navbar}>
                 {
                     menuLinks.map(link =>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,90 +2,17 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import styles from '../components/layout.module.css'
 import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Καλώς ήλθατε στο Prizy!</h1>
-    <p>
-      Ελάτε παρέα να δημιουργήσουμε ένα ωραίο website για να στεγάσει το Landing
-      Page του Prizy ως μέρος του Hacktoberfest.
-    </p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <img src="https://hacktoberfest.digitalocean.com/assets/hacktoberfest-2018-social-card-c8d2e1489f647f2e0a26e6f598adeb760872818905b34cd437afc7ac2857ceab.png" />
+    <div className={styles.container}>
+      <div className={styles.textbox}>
+        <h1>Larissa Developers is a group of awesome people</h1>
+        <h2>who like to do awesome things</h2>
+      </div>
     </div>
-    <p>
-      Projects για το Prizy:
-      <ul>
-        <li>
-          <a
-            href="https://github.com/Larissa-Developers/prizy_backend"
-            target="_blank"
-          >
-            Prizy back-end
-          </a>{' '}
-          |{' '}
-          <a href="https://trello.com/b/zIPr3q6l/prizy-backend" target="_blank">
-            Trello Board
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/Larissa-Developers/prizy_mobile"
-            target="_blank"
-          >
-            Prizy mobile apps
-          </a>{' '}
-          |{' '}
-          <a href="https://trello.com/b/oCVfuIUL/prize-app" target="_blank">
-            Trello Board
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/Larissa-Developers/prizy_landing"
-            target="_blank"
-          >
-            Prizy landing page
-          </a>{' '}
-          (what you see here) |{' '}
-          <a href="https://trello.com/b/rJL4xrcH/prizy-web" target="_blank">
-            Trello Board
-          </a>
-        </li>
-      </ul>
-      <blockquote>
-        Στα Trello boards μπορείτε επιλέξετε τι θα θέλατε να κάνετε από τα
-        αντίστοιχα tasks.
-      </blockquote>
-    </p>
-    <p>
-      Για την επικοινωνία και τον συντονισμό της όμαδας χρησιμοποιούμε το{' '}
-      <strong>#prizy</strong> channel στο Slack. Αν δεν είσαι ακόμη στο Slack
-      κάνε join{' '}
-      <a
-        href="https://larissadevelopers.slack.com/join/shared_invite/enQtMzc1NjYzODc1MjY1LWI3MTM4YjAwZTBkMzAxYjJmYzY0ZTY1MTE3NmFjY2IzMTVjZDllNDczNjk5ZmE0MTY1Yjk1Zjc0NzJkMGY4YzE"
-        target="_blank"
-      >
-        εδώ
-      </a>
-      .
-    </p>
-    <p>
-      Για όσους βλέπετε το GitHub για πρώτη φόρα και δεν ξέρετε τι είναι το Pull
-      Request (PR) ώστε να συνεισφέρετε στο Project, μπορείτε να διαβάσετε
-      περισσότερα{' '}
-      <a
-        href="https://www.digitalocean.com/community/tutorial_series/an-introduction-to-open-source"
-        target="_blank"
-      >
-        εδώ
-      </a>
-      .
-    </p>
-
-    <hr />
-    <Link to="/page-2/">Go to page 2 (example page)</Link>
   </Layout>
 )
 
